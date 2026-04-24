ba0c90e29f4d14edbc66be8a0dc6abf538b4d0e7
ruvish990 <ruvish990@gmail.com>
2026-04-24 15:16:05 +0530

    Initial Spring MVC setup

---
+++ src/main/java/com/example/course_resgitration_system/CourseResgitrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.course_resgitration_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseResgitrationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseResgitrationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/course_resgitration_system/CourseResgitrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course_resgitration_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseResgitrationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
